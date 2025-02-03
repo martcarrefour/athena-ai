bb6fd21e9d68fa43caed004d5c8a59d4e9885444
Dmitriy Prokushev <martcarrefour@gmail.com>
2025-02-03 16:50:52 +0300

    test: tsconfig paths

--- src/Athena.ts
+++ src/Athena.ts
@@ -8,26 +8,8 @@ import {
   Message,
   StreamChunk,
 } from "types";
-
-export function parseMarkdownToJson(md: string): Record<string, unknown> {
-  const jsonMatch = md.match(/```json\n([\s\S]+?)\n```/);
-  if (jsonMatch) {
-    try {
-      return JSON.parse(jsonMatch[1]);
-    } catch (error: unknown) {
-      const errorMessage =
-        error instanceof Error
-          ? error.message
-          : typeof error === "string"
-          ? error
-          : "Unknown error";
-      throw new Error(`Failed to parse JSON: ${errorMessage}`);
-    }
-  }
-  throw new Error(
-    "No valid JSON block found in the provided Markdown content."
-  );
-}
+import "tsconfig-paths/register";
+import { parseMarkdownToJson } from "utils";
 
 export class Athena {
   private driver: ILlmDriver;
